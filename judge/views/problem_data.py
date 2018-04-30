045d543952db610d0e3e3ce8029dfaee0021231e
joonas yoon <joonas.yoon@gmail.com>
2018-04-30 22:56:41 +0900

    테스트 케이스 점수의 기본값 설정

--- judge/views/problem_data.py
+++ judge/views/problem_data.py
@@ -65,7 +65,7 @@ class ProblemCaseForm(ModelForm):
         widgets = {
             'generator_args': HiddenInput,
             'type': Select(attrs={'style': 'width: 100%'}),
-            'points': NumberInput(attrs={'style': 'width: 4em'}),
+            'points': NumberInput(attrs={'style': 'width: 4em', 'value': '5'}),
             'output_prefix': NumberInput(attrs={'style': 'width: 4.5em'}),
             'output_limit': NumberInput(attrs={'style': 'width: 6em'}),
             'checker_args': HiddenInput,
